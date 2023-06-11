28eb67ca77b5c03d04961a97a6f0b5f23d791369
Pradeep <pknitn@gmail.com>
2023-06-11 21:34:59 +0530

    sending mail through java code

---
+++ src/main/java/org/pk/CustomizedMailAuthentication.java
@@ -0,0 +1,10 @@
+package org.pk;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+public class CustomizedMailAuthentication extends Authenticator {
+    protected PasswordAuthentication getPasswordAuthentication()
+    {
+        return new PasswordAuthentication(MailMetaData.myUserMail,MailMetaData.password);
+    }
+}
---
+++ src/main/java/org/pk/MailMetaData.java
@@ -0,0 +1,14 @@
+package org.pk;
+
+public class MailMetaData {
+    public static final String HostServer = "smtp.gmail.com";
+    public static final String port = "465";
+    public static final String sslProperty = "mail.smtp.ssl.enable";
+    public static final String authPerm = "mail.smtp.auth";
+
+    public static final String myUserMail = "pknitn@gmail.com";
+
+    public static final String password = "qlnuoyigqrwdmobb";// app password
+
+    public static final String receiverMail = "pradeepsingh08101998@gmail.com";
+}
